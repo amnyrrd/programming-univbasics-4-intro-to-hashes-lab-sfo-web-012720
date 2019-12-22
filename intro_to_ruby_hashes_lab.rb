bd841bde24078acfa79187368f64217ee0c17055
Aaron Minyard <amnyrrd@gmail.com>
2019-12-22 22:40:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key] == nil
-    return hash[key] = 1 
+    hash[key] = 1 
   else 
-    return hash[key] += 1
+    hash[key] += 1
   end
   return hash
 end
